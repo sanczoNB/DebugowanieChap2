427cb9ddc8349029ae10724c5e1e9725e4362ded
sanczoNB <sanczo16@gmail.com>
2017-01-09 14:39:34 +0100

    Podstawowa wersja programu z błędem do wyśledzenia

--- Debugowanie/Program.cs
+++ Debugowanie/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Debugowanie
+﻿using System;
+
+namespace Debugowanie
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            int x = 1234;
+            int y = Kwadrat(x);
+            y = Kwadrat(y);
+            string sy = y.ToString();
+            Console.WriteLine(sy);
+        }
 
+        private static int Kwadrat(int argument)
+        {
+            var value = argument*argument;
+            return value;
         }
     }
 }
